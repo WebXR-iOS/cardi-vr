90b54ac3a2b5150f4985325fe9c5e01a792d4100
CrazyH <87752773+CrazyH2@users.noreply.github.com>
2024-03-25 07:30:58 +0000

    fix

--- src/screens/start/actionsInfo.js
+++ src/screens/start/actionsInfo.js
@@ -23,10 +23,12 @@ class ActionsInfoScreen extends Screen {
         // Ground
         const mesh = new THREE.Mesh( new THREE.PlaneGeometry( 2000, 2000 ), new THREE.MeshPhongMaterial( { color: 0x999999, depthWrite: false } ) );
 		mesh.rotation.x = - Math.PI / 2;
+        mesh.position.y = -2;
 		mesh.receiveShadow = true;
 		renderGroup.add( mesh );
 
 		const grid = new THREE.GridHelper( 2000, 20, 0x000000, 0x000000 );
+        grid.position.y = -2;
 		grid.material.opacity = 0.2;
 		grid.material.transparent = true;
 		renderGroup.add( grid );
