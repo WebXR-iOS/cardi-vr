a891c37890c2a8e61b3ed6e0381d005257b7e9e4
CrazyH <87752773+CrazyH2@users.noreply.github.com>
2024-03-26 08:11:42 +0000

    remove ui

--- src/screens/start/actionsInfo.js
+++ src/screens/start/actionsInfo.js
@@ -21,37 +21,12 @@ class ActionsInfoScreen extends Screen {
 		renderGroup.fog = new THREE.Fog( 0x976dc2, 200, 1000 );
 
         // Ground
-        /*const mesh = new THREE.Mesh( new THREE.PlaneGeometry( 2000, 2000 ), new THREE.MeshPhongMaterial( { color: 0x999999, depthWrite: false } ) );
-		mesh.rotation.x = - Math.PI / 2;
-        mesh.position.y = -2;
-		mesh.receiveShadow = true;
-		renderGroup.add( mesh );*/
-
 		const grid = new THREE.GridHelper( 2000, 20, 0x000000, 0x000000 );
         grid.position.y = -300;
 		renderGroup.add( grid );
 
         // UI
-        const container = new ThreeMeshUI.Block({
-            width: 1.2,
-            height: 0.7,
-            padding: 0.2,
-            justifyContent: "center",
-            textAlign: "center",
-            fontFamily: './assets/fonts/Roboto-msdf.json',
-            fontTexture: './assets/fonts/Roboto-msdf.png',
-        });
-           
-        const text = new ThreeMeshUI.Text({
-            content: "Welcome to CardiVR!",
-            fontSize: 0.125
-        });
-           
-        container.add( text );
 
-        container.position.z = -1;
-
-        renderGroup.add( container );
     };
 };
 
