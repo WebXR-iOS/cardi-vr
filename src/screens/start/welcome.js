18b3ab6ba9f4ae9e0517a0da119c0faef8e22711
CrazyH <87752773+CrazyH2@users.noreply.github.com>
2024-03-30 09:51:54 +0000

    Update welcome.js

--- src/screens/start/welcome.js
+++ src/screens/start/welcome.js
@@ -34,7 +34,7 @@ class WelcomeScreen extends Screen {
 		texture.minFilter = THREE.LinearFilter;
 		texture.needsUpdate = true;
 
-        var geometry = new THREE.PlaneGeometry(5, (4 / 3));
+        var geometry = new THREE.PlaneGeometry((15 / 6), (4 / 6));
 		var material = new THREE.MeshBasicMaterial( 
 		  { side:THREE.DoubleSide, map:texture, transparent:true, opacity:1.0 } );
 
